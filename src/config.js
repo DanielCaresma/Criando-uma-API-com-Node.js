3f834c41b220423a83bb89823d3e5202cad3cba2
Daniel Caresma Silva <daniel.caresma@gmail.com>
2022-04-05 20:42:31 -0300

    criando service de email

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e578e5761ef';
-global.EMAIL_TMPL = '<strong>{0}</strong>';
+global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo à Node Store!';
 
 module.exports = {
     connetionString: 'mongodb+srv://daniel:db.493544@maquina-sp.suitj.mongodb.net/maquina-sp?retryWrites=true&w=majority',
